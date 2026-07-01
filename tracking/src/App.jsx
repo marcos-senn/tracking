7a24fdfac9c9bbfbc41f8b5de72fed19dea79057
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2026-07-01 16:54:20 -0300

    new styles

--- tracking/src/App.jsx
+++ tracking/src/App.jsx
@@ -5,9 +5,8 @@ import Layout from './components/Layout';
 import DashboardPage from './pages/DashboardPage';
 import DriversPage from './pages/DriversPage';
 import LoadsPage from './pages/LoadsPage';
-import ResumePage from './pages/ResumePage';
+import ResumePage from './pages/ResumePage'; // <-- Importante
 
-// Creamos una pantalla de login simple que se mostrará dentro de tu app
 function SignInPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -23,26 +22,21 @@ export default function App() {
     <BrowserRouter>
       <Toaster />
       <Routes>
-        <Route path="/resume" element={<ResumePage />} />
-        
-        {/* Ruta de Login */}
         <Route path="/login" element={<SignInPage />} />
 
-        {/* Rutas protegidas */}
         <Route path="/*" element={
           <>
             <SignedOut>
-              {/* Si no está logueado, lo manda a /login */}
               <Navigate to="/login" replace />
             </SignedOut>
             
             <SignedIn>
-              {/* Si está logueado, muestra la app normal */}
-              <Layout>
+              <Layout> {/* <-- Todo debe estar dentro de Layout */}
                 <Routes>
                   <Route path="/" element={<DashboardPage />} />
                   <Route path="/drivers" element={<DriversPage />} />
                   <Route path="/loads" element={<LoadsPage />} />
+                  <Route path="/resume" element={<ResumePage />} /> {/* <-- Aquí dentro */}
                   <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
               </Layout>
